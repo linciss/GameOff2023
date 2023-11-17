43b6d6b5ef6853b1dba7506f287d0daa763f7821
Reinis <reinisemilsa@gmail.com>
2023-11-17 21:42:22 +0200

    adding proper iso movement

--- entities/player/Player.cs
+++ entities/player/Player.cs
@@ -3,40 +3,56 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    [Export]
+    private float Speed = 5.0f;
+    [Export]
+    private float JumpVelocity = 4.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    [Export]
+    private Camera3D camera;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+    public override void _Ready()
+    {
+        camera = GetNode<Camera3D>("Camera3D");
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y -= gravity * (float)delta;
+
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+
+        Transform3D camXform = camera.GlobalTransform;
+        Vector3 cameraForward = -camXform.Basis.Z; // Assuming camera's forward axis is -z.
+        Vector3 cameraRight = camXform.Basis.X;    // Assuming camera's right axis is x.
+
+        Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_down", "ui_up");
+        Vector3 input = cameraForward * inputDir.Y + cameraRight * inputDir.X;
+        input.Y = 0; // Disregard vertical movement.
+
+        if (input.LengthSquared() > 0)
+        {
+            input = input.Normalized() * Speed;
+            velocity = input;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+
+        MoveAndSlide();
+    }
 }
--- scenes/Asteroid/grid_map.cs
+++ scenes/Asteroid/grid_map.cs
@@ -11,5 +11,6 @@ public partial class grid_map : GridMap
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 }
