5210eb993d059c804514a390d3d8ffc1354873dd
Reinis <reinisemilsa@gmail.com>
2023-11-17 19:36:52 +0200

    structure fr fr

--- scenes/Asteroid/HelloWorld.cs
+++ scenes/Asteroid/HelloWorld.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
 
-public partial class HelloWorld : Node2D
+public partial class HelloWorld : Node3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GD.Print("Hello World!");
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
